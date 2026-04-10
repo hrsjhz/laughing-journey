0235cd0210e3d4f11bc13b9de64dbd053057e109
hrsjhz <2015928073@qq.com>
2026-04-10 16:52:27 +0800

    Add files via upload

---
+++ TestFightingGame1.1/src/App.java
@@ -0,0 +1,13 @@
+import haoran.domain.User;
+import haoran.ui.FightGame;
+import haoran.ui.Login;
+
+public class App {
+    public static void main(String[] args) {
+        Login log = new Login();
+        log.Start();
+//        User u = new User("浩然","1");
+//        FightGame f = new FightGame();
+//        f.gameStart(u);
+    }
+}
---
+++ TestFightingGame1.1/src/haoran/ui/FightGame.java
@@ -0,0 +1,343 @@
+package haoran.ui;
+
+import haoran.domain.*;
+import haoran.domain.Character;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class FightGame {
+    public void gameStart(User user) {
+        System.out.println("欢迎" + user.getUsername() + "进入游戏");
+        System.out.println("您的id为" + user.getId());
+
+        ArrayList<Skill> skills = new ArrayList<>();
+        skills.add(new Skill("普通攻击",  ""));
+        skills.add(new Skill("强力一击", "(20MP)"));
+        skills.add(new Skill("生命汲取",  "(20MP 恢复30%血量)"));
+        skills.add(new Skill("蓝量回复", "(恢复50%蓝量)"));
+
+        HeroCharacter player = createPlayerCharacter(user.getUsername(),skills);
+        player.show();
+
+        ArrayList<EnemyCharacter> enemyCharacters = createEnemyCharacter();
+        int count = 1;//当前第几场战斗
+        int wins = 0;//当前胜场
+
+        while (player.isSurvive()) {
+            EnemyCharacter enemy;
+
+            if (count % 4 == 1 && count != 1) {
+                for (int i = 0; i < enemyCharacters.size(); i++) {
+                    int addHP = 150;
+                    int addAttack = 25;
+                    int addDefence = 25;
+                    if (i == 3) {
+                        addHP = 200;
+                        addAttack = 30;
+                        addDefence = 30;
+                    }
+
+                    EnemyCharacter c = enemyCharacters.get(i);
+                    c.setMaxHP(c.getMaxHP() + addHP);
+                    c.setHP(c.getMaxHP());
+                    c.setMP(c.getMaxMP());
+                    c.setAttack(c.getAttack() + addAttack);
+                    c.setDefence(c.getDefence() + addDefence);
+                    c.setDefending(false);
+                }
+
+            }//每一轮结束强化enemy
+
+            if(wins % 4 == 3){
+                if (player.getMaxMP() < 200) {
+                    System.out.println();
+                    System.out.println("升级！获得 50 点最大生命值, 10 点攻击力和防御力, 20 点最大蓝量");
+                    player.setMaxHP(player.getMaxHP() + 50);
+                    player.setMaxMP(player.getMaxMP() + 20);
+                    player.setAttack(player.getAttack() + 10);
+                    player.setDefence(player.getDefence() + 10);
+                    player.heal(50);
+                    player.healMP(20);
+                }else{
+                    System.out.println();
+                    System.out.println("升级！获得 50 点最大生命值, 10 点攻击力和防御力，恢复20点蓝量");
+                    player.setMaxHP(player.getMaxHP() + 50);
+                    player.setAttack(player.getAttack() + 10);
+                    player.setDefence(player.getDefence() + 10);
+                    player.heal(50);
+                    player.healMP(20);
+                }
+                player.show();
+            }//升级
+
+            if(wins % 4 == 0 && wins != 0){
+                if (player.getMaxMP() < 200) {
+                    System.out.println();
+                    System.out.println("升级！获得 50 点最大生命值, 20 点攻击力和防御力, 20 点最大蓝量");
+                    player.setMaxHP(player.getMaxHP() + 50);
+                    player.setMaxMP(player.getMaxMP() + 20);
+                    player.setAttack(player.getAttack() + 20);
+                    player.setDefence(player.getDefence() + 20);
+                }else{
+                    System.out.println();
+                    System.out.println("升级！获得 75 点最大生命值, 25 点攻击力和防御力");
+                    player.setMaxHP(player.getMaxHP() + 75);
+                    player.setAttack(player.getAttack() + 25);
+                    player.setDefence(player.getDefence() + 25);
+                }
+                System.out.println("检测到你击败了你马哥,肯定是运气,但还是帮你恢复状态吧");
+                player.setHP(player.getMaxHP());
+                player.setMP(player.getMaxMP());
+                showBar(player);
+                player.show();
+                System.out.println("当前胜场：" + wins);
+            }//boss战后升级
+
+            //选择敌人
+            if (count % 4 == 0) {
+                enemy = enemyCharacters.get(3);
+            } else {
+                enemy = enemyCharacters.get(count % 4 - 1);
+            }
+
+            player.setMPHealing(false);
+            System.out.println();
+            System.out.println("第" + count + "场战斗开始,敌人：「 " + enemy.getName() + " 」");
+            enemy.show();
+            System.out.println("--------------------------------------");
+
+            int round = 1;
+            while (true) {
+                System.out.println();
+                System.out.println("第" + round + "回合开始");
+                showBar(player);
+                showBar(enemy);
+
+                playerRound(player, enemy);
+                if(!enemy.isSurvive()){
+                    System.out.println("你战胜了 "+enemy.getName()+" !");
+                    count++;
+                    wins++;
+                    if(wins % 4 == 0 && wins != 0){
+                        break;
+                    }
+                    System.out.println("当前胜场为： " + wins);
+                    break;
+                }
+                enemyTurn(enemy, player);
+                if(!player.isSurvive()){
+                    System.out.println("你的决心碎了一地....");
+                    System.out.println("浩然真是杂鱼啊~");
+                    break;
+                }
+                round++;
+            }
+        }
+    }
+
+
+    public HeroCharacter createPlayerCharacter(String username,ArrayList<Skill> skills) {
+        int points = 20;
+        HeroCharacter player = new HeroCharacter();
+
+        System.out.println("正在创建您的角色中");
+        System.out.println("您的初始数值为：");
+        System.out.println("生命值 100/100 (每点天赋 + 20 HP）");
+        System.out.println("法力值 100/100 ");
+        System.out.println("攻击力 10      (每点天赋 + 10 ATK）");
+        System.out.println("防御力 10      (每点天赋 + 5 DEF）");
+        System.out.println("(不推荐攻击力5点以下)");
+        System.out.println("--------------------------------------");
+        String[] attributes = {"生命值", "攻击力", "防御力"};
+        int[] values = new int[attributes.length];
+
+        for (int i = 0; i < attributes.length; i++) {
+            System.out.print("分配点数到 " + attributes[i] + " (剩余天赋点：" + points + ")" + ": ");
+            Scanner sc = new Scanner(System.in);
+            values[i] = sc.nextInt();
+            if (values[i] > points) {
+                System.out.println("天赋点数不足,自动将剩下全部点数分配给该项");
+                values[i] = points;
+            }
+            if (values[i] < 0) {
+                System.out.println("输入点数错误！自动分配0点");
+                values[i] = 0;
+            }
+            points -= values[i];
+        }
+        if (points > 0) {
+            System.out.println("剩余的天赋点数已自动分配给生命值");
+            values[0] += points;
+        }
+
+        player.setName(username);
+        player.setHP(100 + values[0] * 20);
+        player.setMaxHP(100 + values[0] * 20);
+        player.setMP(100);
+        player.setMaxMP(100);
+        player.setAttack(10 + values[1] * 10);
+        player.setDefence(10 + values[2] * 5);
+        for (int i = 0; i < skills.size(); i++) {
+            player.skills[i] = skills.get(i);
+        }
+
+
+        System.out.println("创建成功！");
+        System.out.println();
+        return player;
+    }
+
+    public ArrayList<EnemyCharacter> createEnemyCharacter() {
+        ArrayList<EnemyCharacter> enemyCharacters = new ArrayList<>();
+        enemyCharacters.add(new EnemyCharacter("三金", 200, 100, 50, 50, "猛击"));
+        enemyCharacters.add(new EnemyCharacter("本博", 150, 100, 70, 30, "快速攻击"));
+        enemyCharacters.add(new EnemyCharacter("昊泽", 250, 100, 40, 60, "防御姿态"));
+        enemyCharacters.add(new EnemyCharacter("你马哥", 300, 100, 70, 70, "天动万象"));
+        return enemyCharacters;
+    }
+
+    public void showBar(Character c){
+        int count = 20;
+        System.out.print(c.getName() + ": [");
+        int barCount = (int)( (c.getHP() * 1.0 / c.getMaxHP()) * count );
+        for (int i = 0; i < count; i++) {
+            if (i < barCount) {
+                System.out.print("█");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("] " + c.getHP() + "/" + c.getMaxHP() + " HP");
+        System.out.println("\t"+c.getMP() + "/" + c.getMaxMP() + " MP");
+    }
+
+    public int calculateDamage(int attack, int defence){
+        int damage = attack - defence;
+        if (damage < 1) {
+            damage = 1;
+        }
+        return damage;
+    }
+
+    public void playerRound(HeroCharacter player, EnemyCharacter enemy){
+        System.out.println("---你的回合---");
+        System.out.println("请选择行动：");
+        for (int i = 0; i < player.skills.length; i++) {
+            if(player.skills[i] == null){
+                break;
+            }
+            System.out.println(i + 1 + ". " + player.skills[i].getName() + player.skills[i].getDescription());
+        }
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice){
+            case 1 -> {
+                int damage1 = calculateDamage((int) (player.getAttack() * 1.5), enemy.getDefence());
+                System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 普通攻击 ,造成 " + enemy.takeDamage(damage1) + " 点伤害");
+            }
+            case 2 -> {
+                if(player.getMP() >= 20){
+                    player.setMP(player.getMP() - 20);
+                    int damage2 = calculateDamage((int) (player.getAttack() * 1.8), enemy.getDefence());
+                    System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 强力一击 ,造成 " + enemy.takeDamage(damage2) + " 点伤害");
+                }else {
+                    System.out.println("当前法力值不足,自动释放普通攻击");
+                    int damage1 = calculateDamage((int) (player.getAttack() * 1.5), enemy.getDefence());
+                    System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 普通攻击 ,造成 " + enemy.takeDamage(damage1) + " 点伤害");
+                }
+            }
+            case 3 -> {
+                if(player.getMP() >= 20){
+                    player.setMP(player.getMP() - 20);
+                    int damage3 = calculateDamage((int) (player.getAttack() * 1.6), enemy.getDefence());
+                    int heal = (int) (player.getMaxHP() * 0.3);
+                    System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 生命汲取 ,造成了 "+ enemy.takeDamage(damage3) +" 点伤害,恢复了 " + heal + " 点生命值");
+                    player.heal(heal);
+                }else {
+                    System.out.println("当前法力值不足,自动释放普通攻击");
+                    int damage1 = calculateDamage((int) (player.getAttack() * 1.5), enemy.getDefence());
+                    System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 普通攻击 ,造成 " + enemy.takeDamage(damage1) + " 点伤害");
+                }
+            }
+            case 4 -> {
+                if(player.isMPHealing()){
+                    System.out.println("本回合已使用过该技能,自动释放普通攻击");
+                    int damage1 = calculateDamage((int) (player.getAttack() * 1.5), enemy.getDefence());
+                    System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 普通攻击 ,造成 " + enemy.takeDamage(damage1) + " 点伤害");
+                }else{
+                    player.setMPHealing(true);
+                    int healMP = (int) (player.getMaxMP() * 0.5);
+                    System.out.println(player.getName()+" 使用了 蓝量回复 , 恢复了 " + healMP + " 点蓝量");
+                    player.healMP(healMP);
+                }
+            }
+            default -> {
+                System.out.println("无效选择,自动释放普通攻击");
+                int damage1 = calculateDamage((int) (player.getAttack() * 1.5), enemy.getDefence());
+                System.out.println(player.getName()+" 对 " + enemy.getName() + " 使用了 普通攻击 ,造成 " + enemy.takeDamage(damage1) + " 点伤害");
+            }
+        }
+    }
+
+    private void enemyTurn(EnemyCharacter enemy, HeroCharacter player) {
+        System.out.println("---敌人的回合---");
+        String action = "普通攻击";
+
+        Random r = new Random();
+        int num = r.nextInt(10);
+        if(num > 5){
+            action = enemy.getSkill();
+        }
+
+        switch (action) {
+            case "普通攻击"->{
+                int damage1 = calculateDamage((int) (enemy.getAttack()*1.3), player.getDefence());
+                System.out.println(enemy.getName()+" 对 " + player.getName() + " 使用了 普通攻击 ,造成 " + player.takeDamage(damage1) + " 点伤害");
+            }
+            case "猛击"->{
+                if(enemy.getMP() >= 20){
+                    enemy.setMP(enemy.getMP() - 20);
+                    int damage2 = calculateDamage((int) (enemy.getAttack() * 1.5), player.getDefence());
+                    System.out.println(enemy.getName() + " 对 " + player.getName() + " 使用了 猛击 ,造成 " + player.takeDamage(damage2) + " 点伤害");
+                }else {
+                    System.out.println(enemy.getName() + "使用了 蓝量回复");
+                    enemy.healMP(20);
+                }
+            }
+            case "快速攻击"->{
+                if (enemy.getMP() >= 20) {
+                    enemy.setMP(enemy.getMP() - 20);
+                    int damage3 = calculateDamage((int) (enemy.getAttack() * 1.6), player.getDefence());
+                    System.out.println(enemy.getName()+" 对 " + player.getName() + " 使用了 快速攻击 ,造成 2 次" + player.takeDamage(damage3) / 2 + " 点伤害");
+                }else {
+                    System.out.println(enemy.getName() + "使用了 蓝量回复");
+                    enemy.healMP(20);
+                }
+            }
+            case "防御姿态"->{
+                if (enemy.getMP() >= 20) {
+                    enemy.setMP(enemy.getMP() - 20);
+                    System.out.println(enemy.getName()+" 使用了 防御姿态,下次受到的伤害减半");
+                    enemy.setDefending(true);
+                }else {
+                    System.out.println(enemy.getName() + "使用了 蓝量回复");
+                    enemy.healMP(20);
+                }
+            }
+            case "天动万象"->{
+                if (enemy.getMP() >= 50) {
+                    enemy.setMP(enemy.getMP() - 50);
+                    int damage = calculateDamage((int) (enemy.getAttack() * 2.0), player.getDefence());
+                    int heal = (int) (enemy.getMaxHP() * 0.3);
+                    System.out.println(enemy.getName()+" 对 " + player.getName() + " 使用了 天动万象 ,造成 " + player.takeDamage(damage) + " 点伤害,恢复了 "+ heal +"点生命值");
+                    enemy.heal(heal);
+                }else {
+                    System.out.println(enemy.getName() + "使用了 蓝量回复");
+                    enemy.healMP(25);
+                }
+            }
+        }
+    }
+}
